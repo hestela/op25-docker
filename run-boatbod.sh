339ce1e088d43a4362de19bf6bcb571148613469
Henry Estela <henryestela@gmail.com>
2020-06-08 15:39:20 -0700

    Remove extra docker flags

--- run-boatbod.sh
+++ run-boatbod.sh
@@ -1,6 +1,6 @@
 xhost +local:root
 docker run -it --device /dev/bus/usb -e PULSE_SERVER=pulseaudio \
   -v "/tmp/.X11-unix:/tmp/.X11-unix:rw" --env="DISPLAY" \
-  --env="QT_X11_NO_MITSHM" --net=host \
-  -v "$HOME/.Xauthority:/root/Xauthority:rw" --device /dev/snd op25-boatbod
+  --env="QT_X11_NO_MITSHM" \
+  --device /dev/snd op25-boatbod
 xhost -local:root
